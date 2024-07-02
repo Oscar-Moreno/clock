865ad00dfb9dfeb306c7e5b8f8c63d0803ae077e
oscar1630 <osmolomax@gmail.com>
2024-07-02 10:54:15 +0200

    dfsdf

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const digitalHour = document.getElementById('digital-hour')
+const digitalMinutes = document.getElementById('digital-minutes')
+const digitalWeekDay = document.getElementById('week-day')
+const digitalMonthDay = document.getElementById('month-day')
+const digitalMonth = document.getElementById('month')
+const digitalYear = document.getElementById('year')
+
+
+const changeDate = () =>{
+    const date = new Date()
+
+    digitalHour.textContent = date.getHours()
+    digitalMinutes.textContent = date.getMinutes()
+    console.log(date)
+}
