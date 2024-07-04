776d5dac42c5a919c844aa7c50a76194d404fa58
oscar1630 <osmolomax@gmail.com>
2024-07-04 09:44:01 +0200

    reloj-digital

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,74 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const dayOfWeekElement = document.getElementById('day-of-week');
+const dayElement = document.getElementById('day');
+const monthElement = document.getElementById('month');
+const yearElement = document.getElementById('year');
+const hoursHandElement = document.getElementById('hour-hand');
+const minutesHandElement = document.getElementById('minute-hand');
+const secondsHandElement = document.getElementById('second-hand');
 
-const digitalHour = document.getElementById('digital-hour')
-const digitalMinutes = document.getElementById('digital-minutes')
-const digitalWeekDay = document.getElementById('week-day')
-const digitalMonthDay = document.getElementById('month-day')
-const digitalMonth = document.getElementById('month')
-const digitalYear = document.getElementById('year')
+const days = [
+  'Sunday',
+  'Monday',
+  'Tusday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
 
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
 
-const changeDate = () =>{
-    const date = new Date()
+const fixTime = number => (number < 10 ? '0' + number : number);
+const getDayOfWeek = today => days[today];
+const getMonth = currentMonth => months[currentMonth];
 
-    digitalHour.textContent = date.getHours()
-    digitalMinutes.textContent = date.getMinutes()
-    console.log(date)
-}
+const setClocks = () => {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  const today = now.getDay();
+  const date = now.getDate();
+  const currentMonth = now.getMonth();
+  const currentYear = now.getFullYear();
+
+  hoursElement.textContent = fixTime(hours);
+  minutesElement.textContent = fixTime(minutes);
+
+  dayOfWeekElement.textContent = getDayOfWeek(today);
+  dayElement.textContent = date;
+  monthElement.textContent = getMonth(currentMonth);
+  yearElement.textContent = currentYear;
+
+  const secondsDegrees = seconds * 6;
+  secondsHandElement.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutesDegrees = minutes * 6 + seconds / 10;
+  minutesHandElement.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hourDegrees = hours * 30 + minutes / 2;
+  hoursHandElement.style.transform = `rotate(${hourDegrees}deg)`;
+};
+
+setInterval(() => {
+  setClocks();
+}, 1000);
+
+setClocks();
